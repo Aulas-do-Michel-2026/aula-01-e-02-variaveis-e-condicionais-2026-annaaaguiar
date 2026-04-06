203fffb806bdb17e291915dff773a99988a39f26
annaaaguiar <annaaguiar.galo@gmail.com>
2026-04-06 18:14:20 -0300

    Implement dosage calculation based on user weight
    Add user input for weight and calculate recommended dosage.

--- exercicio_2.py
+++ exercicio_2.py
@@ -13,3 +13,8 @@ Digite o peso do paciente (em kg):
 Resposta:
 Média: 140 mg
 """
+
+peso = (int(input("Digite eu peso em kg: ")))
+formula = 2 * peso
+
+print(f"A dose recomendada do medicamento é de {formula}mg")
