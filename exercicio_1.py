e7cd58f1962e600cd5899b540798bd177b056881
annaaaguiar <annaaguiar.galo@gmail.com>
2026-04-06 18:08:04 -0300

    Add input handling and average calculation

--- exercicio_1.py
+++ exercicio_1.py
@@ -19,3 +19,14 @@ Dica: Use inputs para receber os dados!
 Lembre de converter ele para o tipo necessário!
 Print na tela com "print"
 """
+
+num1 = (float(input("Digite um número: ")))
+num2 = (float(input("Digite outro número: ")))
+num3 = (float(input("Digite outro número: ")))
+
+print (f"O seu primeiro número foi {num1}")
+print (f"O seu segundo número foi {num2}")
+print (f"O seu terceiro número foi {num3}")
+
+media = (float(num1 + num2 + num3) /3)
+print(f"A média dos 3 números é: {media:.2f}")
